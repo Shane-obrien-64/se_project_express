b42dec5f085ccc08a898b8855c522bf9be214f40
Shane O'Brien <shane.g.obrien@gmail.com>
2023-10-05 10:33:50 -0400

    .env change

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,9 @@
-const JWT_SECRET = "9Zp@-#RtW7$%Y";
+require("dotenv").config();
+const { NODE_ENV, JWT_SECRET } = process.env;
+console.log(JWT_SECRET);
+// const JWT_SECRET = "9Zp@-#RtW7$%Y";
 
 module.exports = {
+  NODE_ENV,
   JWT_SECRET,
 };
